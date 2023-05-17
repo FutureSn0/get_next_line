72ddb949800c00fdfe13ba6ef8f98ba317a060f0
Aaron Pryce <aapryce@a1-r3-p10.s19.be>
2023-05-17 13:50:54 +0200

    alpha stage

---
+++ get_next_line.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aapryce <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/20 11:51:26 by aapryce           #+#    #+#             */
+/*   Updated: 2023/05/17 13:44:25 by aapryce          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include "get_next_line.h"
+
+char	*ft_strcpy(char *dest, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+static int	ft_read(int fd, char **store)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	int		bytes;
+
+	bytes = read(fd, buffer, BUFFER_SIZE - 1);
+	if (bytes == -1)
+		return (-1);
+	buffer[bytes] = '\0';
+	*store = ft_strjoin(*store, buffer);
+	return (bytes);
+}
+
+static char	*ft_extract(char **store)
+{
+	char	*line;
+	char	*next_line;
+
+	next_line = ft_strchr(*store, '\n');
+	if (next_line)
+	{
+		line = ft_substr(*store, 0, next_line - *store + 1);
+		*store = ft_strdup(next_line + 1);
+	}
+	else
+	{
+		line = ft_strdup(*store);
+		*store = NULL;
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*store;
+	char		*line;
+	int			bytes;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!store)
+		store = ft_strdup("");
+	bytes = 1;
+	while (bytes > 0 && !ft_strchr(store, '\n'))
+		bytes = ft_read(fd, &store);
+	if (bytes == -1)
+		return (NULL);
+	if (!ft_strlen(store))
+		return (NULL);
+	line = ft_extract(&store);
+	return (line);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*line;
+	int	fd;
+
+	fd = open("test.txt", O_RDONLY);
+	
+	line = get_next_line(fd);
+	printf("%s", line);
+
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free(line);
+	}
+
+	close(fd);
+	return (0);
+}
+*/
---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aapryce <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/20 11:52:24 by aapryce           #+#    #+#             */
+/*   Updated: 2023/05/16 09:55:25 by aapryce          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "unistd.h"
+# include "stdlib.h"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char *str1, char *str2);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strdup(const char *src);
+char	*ft_strchr(const char *str, int c);
+
+#endif
